257ccae1749af8a803080acf1a165967f50e1f60
akankshamanro <67097920+akankshamanro@users.noreply.github.com>
2025-08-14 21:16:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, jsonify
+from webapp import create_app
+
+# Get the directory of the current script
+#dir_path = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the absolute path of the credentials file in the secrets directory
+#credentials_path = os.path.join(dir_path, 'secrets', 'credentials.json')
+
+# Set the environment variable to the credentials file path
+#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
+app = create_app()
+
+@app.route('/health', methods=['GET'])
+def get_service_health():
+
+    response = {
+      "status": "healthy",     
+      "status_code":200
+                }
+        
+    return jsonify(response), 200
+    
+
+#Create and run the Flask app
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
